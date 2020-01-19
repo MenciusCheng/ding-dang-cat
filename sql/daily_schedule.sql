15d7eb209e2bb7880d7d9de56b26f7bed200393b
程孟威 <295567172@qq.com>
2020-01-19 18:23:30 +0800

    sql

---
+++ sql/daily_schedule.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `daily_schedule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(20) NOT NULL COMMENT '休/班名称',
+  `start_date` date NOT NULL COMMENT '起始日期',
+  `end_date` date NOT NULL COMMENT '截止日期',
+  `work` tinyint(4) NOT NULL COMMENT '是否正常上班,0否/1是',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='休假/调休日程表';
